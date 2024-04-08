e293c268510405edc8081624641994d730be69f9
Enderson Kruger <endersonkruger@hotmail.com>
2024-04-08 17:19:03 -0300

    first commit

---
+++ lua/endersonkruger/init.lua
@@ -0,0 +1,10 @@
+require("endersonkruger.remap")
+require("endersonkruger.set")
+require("endersonkruger.lazyInit")
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.cmd[[colorscheme solarized-osaka]]
+
+print("Welcome back, Sir")
