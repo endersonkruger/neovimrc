8af4bf0ca898d0b3d00bc5fabaf37e3c5729bd62
Enderson Kruger <101151376+endersonkruger@users.noreply.github.com>
2024-04-23 18:06:26 -0300

    Delete init.lua

--- init.lua
+++
@@ -1,9 +0,0 @@
-require("endersonkruger.remap")
-require("endersonkruger.set")
-require("endersonkruger.lazy")
-vim.notify = require("endersonkruger.plugins.notify")
-
-print("Welcome back, Sir")
-require("notify")("Welcome Back, Sir")
-
-vim.cmd[[colorscheme solarized-osaka]]
